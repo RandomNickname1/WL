24212436d661d970e723dadb6cb90654e8ae9b7a
Bedrock <108615906+RandomNickname1@users.noreply.github.com>
2022-07-17 11:19:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
   navigator.serviceWorker.register("sw.js")
     .then(() => console.log("Y"))
-    .catch(() => console.log("N"))
+    .catch(() => console.log("N"));
 }
